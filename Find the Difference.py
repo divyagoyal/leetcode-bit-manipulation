48e354803fc0978a0a4e2f8c19e962392419911c
Divya <divya05goyal@gmail.com>
2020-05-28 21:38:28 +0530

    Create Find the Difference.py

---
+++ Find the Difference.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/find-the-difference/
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        code = 0
+        for ch in s+t:
+            code ^= ord(ch)
+        return chr(code)
+    
